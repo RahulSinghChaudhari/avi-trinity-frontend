318b6bc2be1122d596edb56dc2ded876e6e3eb43
unknown <ithead@allindianitservices.com>
2022-11-17 11:25:52 +0530

    about changes

--- src/Components/aboutUs/AboutInfo.js
+++ src/Components/aboutUs/AboutInfo.js
@@ -1,19 +1,28 @@
 import React from 'react'
+import Footer from '../footer/Footer'
 
 const AboutInfo = () => {
         return (
                 <>
-                        <div className='infoMain'>
-                                <div className='row'>
-                                        <div>
-                                                <img src='/assets/images/logo-removebg-preview.png' alt='info img' />
-                                        </div>
-                                        <div>
+                        <div className='infoMain mt-4' >
+                                <div className='container'>
+                                        <div className='row'>
+                                                <div className='col-md-6'>
+                                                        <div className='imgContainer'>
+                                                                <img src='/assets/images/ying-ge--Yo1cWJVKFY-unsplash.jpg' alt='info img' width="80%" height="400px" />
+                                                        </div>
+                                                </div>
+                                                <div className='col-md-6'>
+                                                        <div className='infoContentContainer'>
+                                                                <p>ncildsjlssjvjjn</p>
+                                                        </div>
+
+                                                </div>
 
                                         </div>
-
                                 </div>
                         </div>
+                        <Footer />
                 </>
         )
 }
